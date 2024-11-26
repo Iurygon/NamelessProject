98683c538ec46d9b22262ca069bc35468b7863cc
Iury Silva <iurygon.silva@gmail.com>
2024-11-25 23:49:30 -0300

    Criação da classe Product

---
+++ classes/client.py
@@ -0,0 +1,8 @@
+class Client():
+    def __init__(self, clientCode, clientName, clientCity, clientState, clientCNPJ,clientPurchaseLimit):
+        self.clientCode = clientCode
+        self.clientName = clientName
+        self.clientCity = clientCity
+        self.clientState = clientState
+        self.clientCNPJ = clientCNPJ
+        self.clientPurchaseLimit = clientPurchaseLimit
---
+++ classes/product.py
@@ -0,0 +1,6 @@
+class Product():
+    def __init__(self, productCode, productName, productPrice):
+        self.productCode = productCode
+        self.productName = productName
+        self.productPrice = productPrice
+        
