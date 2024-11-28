e8e9ec397ce1659179d10c4bbb1a928c4eeb988a
Iury Silva <iurygon.silva@gmail.com>
2024-11-27 23:58:20 -0300

    Criação da classe Invoice e modificação na classe Client

--- classes/client.py
+++ classes/client.py
@@ -1,8 +1,9 @@
 class Client():
-    def __init__(self, clientCode, clientName, clientCity, clientState, clientCNPJ,clientPurchaseLimit):
+    def __init__(self, clientCode, clientName, clientCity, clientState, clientCNPJ):
         self.clientCode = clientCode
         self.clientName = clientName
         self.clientCity = clientCity
         self.clientState = clientState
         self.clientCNPJ = clientCNPJ
-        self.clientPurchaseLimit = clientPurchaseLimit
+
+    
