c4f44e14fa555a0641298740243fa38e867b2712
Iury Silva <iurygon.silva@gmail.com>
2024-12-15 23:59:23 -0300

    Criação do menu de principal do cadastro de notas

--- menus/menuNotas.py
+++ menus/menuNotas.py
@@ -1,5 +1,25 @@
+import os
+
 def menuNotas():
+    os.system("cls")
     print("Menu de notas!\n Selecione qual opção deseja seguir:\n"
           "1 - Lançar notas\n"
+          "2 - Pagar notas em aberto\n"
           "2 - Consultar notas\n"
           "3 - Excluir nota\n")
+    opMenuNotas = input("Selecione qual opção deseja seguir: ")
+    if opMenuNotas not in ("1","2","3","4"):
+        print("O valor digitado deve estar entre as opções acima.")
+        input("Pressione Enter para prosseguir.")
+        menuNotas()
+
+
+#LANÇAR NOTAS
+
+#PAGAR NOTAS EM ABERTO
+
+
+#CONSULTAR NOTAS
+
+
+#EXCLUIR NOTAS
