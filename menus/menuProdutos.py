ef7f91891c6fca92b7fae54f6bf6acff143b9f7c
Iury Silva <iurygon.silva@gmail.com>
2025-01-06 23:58:25 -0300

    Continuação do menu de produtos

--- menus/menuProdutos.py
+++ menus/menuProdutos.py
@@ -2,6 +2,7 @@ import connection as sql
 import os
 
 def menuProdutos():
+    dadosProdutos = sql.cursor.execute("SELECT MAX(CODIGO), * FROM PRODUTOS (NOLOCK)").fetchall()
     os.system("cls")
     print("Menu de produtos!\n Selecione qual opção deseja seguir:\n"
           "1 - Cadastrar novo produto\n"
@@ -14,23 +15,23 @@ def menuProdutos():
         input("Pressione Enter para prosseguir")
         menuProdutos()
     match opMenuProd:
-        case "1": cadastrarProduto()
-        case "2": atualizarCadProduto()
-        case "3": consultarProduto()
-        case "4": excluirProduto()
+        case "1": cadastrarProduto(dadosProdutos)
+        case "2": atualizarCadProduto(dadosProdutos)
+        case "3": consultarProduto(dadosProdutos)
+        case "4": excluirProduto(dadosProdutos)
 
 #CADASTRAR PRODUTO
-def cadastrarProduto():
+def cadastrarProduto(dadosProdutos):
     pass
 
 #ATUALIZAR CADASTRO DE UM PRODUTO
-def atualizarCadProduto():
+def atualizarCadProduto(dadosProdutos):
     pass
 
 #CONSULTAR UM PRODUTO
-def consultarProduto():
+def consultarProduto(dadosProdutos):
     pass
 
 #EXCLUIR UM PRODUTO
-def excluirProduto():
+def excluirProduto(dadosProdutos):
     pass
